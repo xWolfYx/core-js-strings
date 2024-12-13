1f67a766007a0673396242aa3de5ee4c54b4cc02
xWolfYx <david.skliarov@gmail.com>
2024-12-13 17:43:40 +0400

    feat: resolve function getStringLength

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -19,8 +19,8 @@
  *   getStringLength(null) => 0
  *   getStringLength(undefined) => 0
  */
-function getStringLength(/* value */) {
-  throw new Error('Not implemented');
+function getStringLength(value) {
+  return value === null || value === undefined ? 0 : value.length;
 }
 
 /**
