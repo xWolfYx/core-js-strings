5dca361a26d5224ddce0f226ffc3248743d67359
xWolfYx <david.skliarov@gmail.com>
2024-12-13 16:34:26 +0400

    feat: resolve function repeatString

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -130,8 +130,11 @@ function removeTrailingWhitespaces(value) {
  *   repeatString('', 3) => ''
  *   repeatString('abc', -2) => ''
  */
-function repeatString(/* str, times */) {
-  throw new Error('Not implemented');
+function repeatString(str, times) {
+  if (times <= 0) {
+    return '';
+  }
+  return str.repeat(times);
 }
 
 /**
