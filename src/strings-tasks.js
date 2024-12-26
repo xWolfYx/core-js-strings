860dabee65d999bd411251ec0874f5ed095d192c
xWolfYx <david.skliarov@gmail.com>
2024-12-26 14:38:09 +0400

    feat: resolve reverseWords function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -380,8 +380,15 @@ function findLongestWord(/* sentence */) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const reverse = str
+    .split('')
+    .reverse()
+    .join('')
+    .split(' ')
+    .reverse()
+    .join(' ');
+  return reverse;
 }
 
 /**
